cf59d4e6f2a7e764d1e100b7c0be097b525a58d4
Ofelia <oer.rodriguez@gmail.com>
2021-02-03 14:01:45 +0000

    Adding ability to prompt user to add their own restaurant and rating along with seperating out into multiple functions

--- ratings.py
+++ ratings.py
@@ -1,8 +1,8 @@
 """Restaurant rating lister."""
 
-def rate_a_restaurant(filename):
+def add_ratings():
 
-    restaurants = open(filename)
+    restaurants = open("scores.txt")
 
     restaurant_ratings = {}
 
@@ -12,7 +12,20 @@ def rate_a_restaurant(filename):
 
         if establishment[0] not in restaurant_ratings:
             restaurant_ratings[establishment[0]] = establishment[1]
-    
+
+    return restaurant_ratings
+
+def add_restaurant(restaurant_ratings):
+
+    print("Add a rating for a restaurant!")
+    restaurant_name = input("Restaurant name > ").title()
+    restaurant_score = int(input("Restaurant rating > "))
+
+    restaurant_ratings[restaurant_name] = restaurant_score
+
+
+def sorted_scores(restaurant_ratings):
+
     sorted_restaurant = sorted(restaurant_ratings)
 
     for restaurant in sorted_restaurant:
@@ -20,4 +33,11 @@ def rate_a_restaurant(filename):
 
     return (restaurant_ratings)
 
-rate_a_restaurant("scores.txt")
+
+# will process existing restaurants and ratings from file
+restaurant_ratings = add_ratings()
+
+# 
+add_restaurant(restaurant_ratings)
+
+sorted_scores(restaurant_ratings)
