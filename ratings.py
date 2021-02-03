b8d9731f5306b21d2ec21cb9b3092c6b7e49962a
Ofelia <oer.rodriguez@gmail.com>
2021-02-03 05:41:09 +0000

    Returned ratings for establishments in alphabetical order

--- ratings.py
+++ ratings.py
@@ -1,4 +1,23 @@
 """Restaurant rating lister."""
 
+def rate_a_restaurant(filename):
 
-# put your code here
+    restaurants = open(filename)
+
+    restaurant_ratings = {}
+
+    for line in restaurants:
+        line = line.rstrip()
+        establishment = line.split(':')
+
+        if establishment[0] not in restaurant_ratings:
+            restaurant_ratings[establishment[0]] = establishment[1]
+    
+    sorted_restaurant = sorted(restaurant_ratings)
+
+    for restaurant in sorted_restaurant:
+        print(f'{restaurant} is rated at {restaurant_ratings[restaurant]}.')
+
+    return (restaurant_ratings)
+
+rate_a_restaurant("scores.txt")
